759d178a5743c3823689ce2c050c6d28cced2e2d
Siri-chandana22 <siriyarlagadda09@gmail.com>
2025-05-27 20:28:34 +0530

    Update recommendation system.py

--- recommendation system.py
+++ recommendation system.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.metrics import mean_squared_error
 df = pd.read_csv('large_ratings.csv')  
 print("Original Ratings Data:")
 print(df.head())
